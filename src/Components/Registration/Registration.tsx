0a12b41cb38db67e3a8592fb3fc66578c9068c00
Paylak Navasardyan <paylak2002n@gmail.com>
2025-11-10 13:16:16 +0400

    feat: Implement user registration form state management with useReducer

--- src/Components/Registration/Registration.tsx
+++ src/Components/Registration/Registration.tsx
@@ -1,9 +1,46 @@
+import { useReducer } from 'react';
 import classes from './Registration.module.css';
 import { GoChevronRight as GoChevronRightIcon } from "react-icons/go";
 import { Link } from 'react-router-dom';
 
 export default function Registration() {
-   const GoChevronRight = GoChevronRightIcon as unknown as React.FC<{ size?: number; style?: React.CSSProperties }>;
+  const GoChevronRight = GoChevronRightIcon as unknown as React.FC<{ size?: number; style?: React.CSSProperties }>;
+
+  type State = {
+    email: string,
+    userName: string,
+    password: string,
+    confirmPass: string
+  };
+
+  type Action = {
+    name: keyof State,
+    value: string
+  };
+
+  const initialState: State = {
+    email: '',
+    userName: '',
+    password: '',
+    confirmPass: ''
+  };
+
+  function reducer(state: State, action: Action) {
+    return {
+      ...state,
+      [action.name]: action.value
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      name: e.target.name as keyof State,
+      value: e.target.value,
+    });
+  };
+
   return (
     <div className={classes.registration}>
       <div className={classes.registrationBody}>
@@ -15,6 +52,9 @@ export default function Registration() {
               className={classes.registrationFields}
               type="email"
               placeholder="Email"
+              name='email'
+              value={state.email}
+              onChange={handleChange}
               required
             />
           </div>
@@ -24,6 +64,9 @@ export default function Registration() {
               className={classes.registrationFields}
               type="text"
               placeholder="Username"
+              name='username'
+              value={state.userName}
+              onChange={handleChange}
               required
             />
           </div>
@@ -33,6 +76,9 @@ export default function Registration() {
               className={classes.registrationFields}
               type="password"
               placeholder="Password"
+              name='password'
+              value={state.password}
+              onChange={handleChange}
               required
             />
           </div>
@@ -42,6 +88,9 @@ export default function Registration() {
               className={classes.registrationFields}
               type="password"
               placeholder="Confirm Password"
+              name='confirm password'
+              value={state.confirmPass}
+              onChange={handleChange}
               required
             />
           </div>
