30b1615e7a1333655639855d3f27c151050907b6
Paylak Navasardyan <paylak2002n@gmail.com>
2025-12-15 17:45:08 +0400

    feat(reset): Add validation for input fields

--- src/Components/Forgot/ResetPassword.tsx
+++ src/Components/Forgot/ResetPassword.tsx
@@ -1,23 +1,77 @@
 import React, { useState } from 'react'
 import classes from './ResetPassword.module.css'
 import { TbLockPassword } from "react-icons/tb";
-import { keyboard } from '@testing-library/user-event/dist/keyboard';
+import { useNavigate } from 'react-router-dom';
 
 export default function ResetPassword() {
-    const LockPassword = TbLockPassword as unknown as React.FC<{color?: string, size?: string}>;
+  const LockPassword = TbLockPassword as unknown as React.FC<{color?: string, size?: string}>;
 
-    const [password, setPassword] = useState<string>('');
-    const [confirmPass, setComfirmPass] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPass, setComfirmPass] = useState<string>('');
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if(e.key === 'Enter') {
-        e.preventDefault();
-        const form = e.currentTarget.form;
-        const index = Array.prototype.indexOf.call(form, e.currentTarget);
-        const nextInput = form?.[index + 1] as HTMLElement;
-        nextInput?.focus()
+  const [passwordError, setPasswordError] = useState<string>('');
+  const [confirmPassError, setConfirmPassError] = useState<string>('');
+
+  const navigate = useNavigate();
+
+  const validPassword = (password: string): string => {
+    const startsWithLetterRegex = /^[a-zA-Z]/;
+    const passCharactersRegex = /^[a-zA-Z0-9_.-]+$/;
+    let passFirstChar = password.charAt(0).toLowerCase();
+    let passChars = '';
+    
+    if (!startsWithLetterRegex.test(password)) {
+      return 'Password must start with Latin letter';
+    } else if (!passCharactersRegex.test(password)) {
+      return 'Password can contain only Latin letters numbers and (-, _, .)';
+    } else if (password.length < 6) {
+      return 'Password cannot contain fewer than 6 characters';
+    } else if (password.length > 15) {
+      return 'Password cannot contain more than 15 characters';
+    }
+
+    for (let i = 0; i < password.length; i++) {
+      if (password[i].toLowerCase() === passFirstChar) {
+        passChars += password[i];
       }
-    };
+  
+      if (passChars.length === password.length) {
+        return 'Password cannot contain only identical characters';
+      }
+    }
+    return '';
+  };
+
+  const validConfirmPass = (confirmPass: string): string => {
+    if (confirmPass !== password) {
+      return 'Repeat Password does not match with Password';
+    }
+    return '';
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const approvedPasswordError = validPassword(password);
+    const approvedConfirmPassError = validConfirmPass(confirmPass);
+
+    setPasswordError(approvedPasswordError);
+    setConfirmPassError(approvedConfirmPassError);
+
+    if (approvedPasswordError || approvedConfirmPassError) return;
+    
+    navigate('/travel-share');
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') {
+      e.preventDefault();
+      const form = e.currentTarget.form;
+      const index = Array.prototype.indexOf.call(form, e.currentTarget);
+      const nextInput = form?.[index + 1] as HTMLElement;
+      nextInput?.focus()
+    }
+  };
     
   return (
     <div className={classes.reset}>
@@ -29,6 +83,7 @@ export default function ResetPassword() {
 
             <form>
               <div className={classes.inputDiv}>
+                {passwordError && <p className={classes.errorMessage}>{passwordError}</p>}
                 <input 
                   className={classes.resetFields}
                   type='password'
@@ -42,6 +97,7 @@ export default function ResetPassword() {
               </div>
 
               <div className={classes.inputDiv}>
+                {confirmPassError && <p className={classes.errorMessage}>{confirmPassError}</p>}
                   <input 
                     className={classes.resetFields}
                     type='password'
@@ -54,7 +110,7 @@ export default function ResetPassword() {
               </div>
 
                <div className={classes.resetButton}>
-                  <button type="button">
+                  <button type="button" onClick={handleClick}>
                     Reset Password
                   </button>
                 </div>
