a510679b15c3569cb431eb5362cd6e68d0c6f64a
Paylak Navasardyan <paylak2002n@gmail.com>
2025-12-22 21:55:07 +0400

    feat: implement TravelShareLayout with Navbar, SideBar and Outlet

--- src/Components/Main/Layout/TravelShareLayout.tsx
+++ src/Components/Main/Layout/TravelShareLayout.tsx
@@ -1,1 +1,22 @@
-feat: add main layout structure for TravelShare app
+import classes from './TraveShareLayout.module.css'
+import { Outlet } from 'react-router-dom'
+import Home from '../Home/Home'
+import Navbar from '../Navbar/Navbar'
+import SideBar from '../SideBar/SideBar'
+
+export default function TravelShareLayout() {
+  return (
+    <div className={classes.layout}>
+      <Navbar />
+
+      <div className={classes.main}>
+        <SideBar />
+
+        <div className={classes.content}>
+          <Home />
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
