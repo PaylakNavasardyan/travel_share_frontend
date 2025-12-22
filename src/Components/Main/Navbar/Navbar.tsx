075f6af992518c2b0999f24ad1625835cc8f7adf
Paylak Navasardyan <paylak2002n@gmail.com>
2025-12-22 20:55:03 +0400

    refactor: move settings from navbar to sidebar

--- src/Components/Main/Navbar/Navbar.tsx
+++ src/Components/Main/Navbar/Navbar.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import classes from './Navbar.module.css';
 import image from '../../../ExportImages';
 import { GoSearch as GoSearchIcon } from "react-icons/go";
-import { IoMdSettings as IoMdSettingsIcon} from "react-icons/io";
 
 export default function Navbar() {
   const GoSearch = GoSearchIcon as unknown as React.FC<{ className: string, size: number, color: string }>
-  const IoMdSettings = IoMdSettingsIcon as unknown as React.FC<{ className: string, size: number, color: string }>
 
   return (
     <div className={classes.navbar}>
       <div className={classes.navbarBody}>
         <div className={classes.navbarLogo}>
-          <img src={image.logo} alt="logo" />
+          <img className={classes.logo} src={image.logo} alt="logo" />
         </div>
 
         <div className={classes.navbarSearchBar}>
@@ -26,10 +24,6 @@ export default function Navbar() {
 
             <GoSearch className={classes.searchIcon} size={18} color='#9ca3af'/>
           </form>
-        </div>
-
-        <div className={classes.navbarSettings}>
-          <IoMdSettings  className={classes.settingIcon} size={25} color='#4848A0FF'/>
         </div>
       </div>
     </div>
