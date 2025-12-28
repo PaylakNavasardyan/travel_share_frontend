455dfc962faffeac44c864021ee83ffd38713bec
Paylak Navasardyan <paylak2002n@gmail.com>
2025-12-28 19:16:31 +0400

    feat: add modal routing with background location

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Registration from './Components/Registration/Registration';
 import Login from './Components/Login/Login';
 import Forgot from './Components/Forgot/Forgot';
@@ -7,48 +7,32 @@ import ResetPassword from './Components/Forgot/ResetPassword';
 import TravelShareLayout from './Components/Main/Layout/TravelShareLayout';
 import AllPosts from './Components/Main/Posts/AllPosts';
 import FriendsPosts from './Components/Main/Posts/FriendsPosts';
-import MyPosts from './Components/Main/Posts/MyPosts';
+import Follow from './Components/Main/UserControls/Follow/Follow';
 
 function App() {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
+
   return (
     <div className="App">
-      <Routes>
-        <Route 
-          path={'/'}
-          element = {
-            <Registration />
-          }
-        />
-
-        <Route 
-          path={'/user-login'}
-          element = {
-            <Login />
-          }
-        />
-
-        <Route 
-          path={'/forgot-password'}
-          element = {
-            <Forgot />
-          }
-        />
-
-        <Route 
-          path={'/reset-password'}
-          element = {
-            <ResetPassword />
-          }
-        />
-
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path="/" element={<Registration />} />
+        <Route path="/user-login" element={<Login />} />
+        <Route path="/forgot-password" element={<Forgot />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
         <Route path="/travel-share" element={<TravelShareLayout />}>
           <Route index element={<AllPosts />} />
           <Route path="all-posts" element={<AllPosts />} />
           <Route path="friends-posts" element={<FriendsPosts />} />
-          <Route path="my-posts" element={<MyPosts />} />
         </Route>
       </Routes>
+
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route path="/follow" element={<Follow />} />
+        </Routes>
+      )}
     </div>
   );
 }
