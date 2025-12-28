726125bf36bab1f6db64f22e56df0971d2b7f37e
Paylak Navasardyan <paylak2002n@gmail.com>
2025-12-28 19:58:47 +0400

    feat: add Following modal overlay

---
+++ src/Components/Main/UserControls/Follow/Following.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import classes from './Followers.module.css';
+import { IoMdClose as IoMdCloseIcon } from "react-icons/io";
+import { GoSearch as GoSearchIcon } from "react-icons/go";
+
+export default function Follow() {
+  const IoMdClose = IoMdCloseIcon as unknown as React.FC<{ className: string }>
+  const GoSearch = GoSearchIcon as unknown as React.FC<{ className: string}>
+  const navigate = useNavigate();
+
+  const handleClick = ():void => {
+    navigate(-1);
+  };
+
+  return (
+    <div className={classes.modal}>
+      <div className={classes.modalBody}>
+        <div className={classes.goBack} onClick={handleClick}>
+          <IoMdClose className={classes.XIcon}/>
+        </div>
+
+        <div className={classes.modalGuide}>
+          <span>Following</span>
+
+          <input 
+            className={classes.inputPool}
+            type='text'
+            placeholder='Search'
+            name='search'
+          />
+
+          <GoSearch className={classes.searchIcon} />
+        </div>
+
+        <div className={classes.modalLine}></div>
+      </div>
+    </div>
+    
+  );
+}
