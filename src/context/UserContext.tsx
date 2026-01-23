8539e549254e9aa209a97a59cb3daa6bbcaca495
Paylak Navasardyan <paylak2002n@gmail.com>
2026-01-23 19:33:11 +0400

    feat: implement UserContext for global state management

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type User = {
+  userName: string;
+  email: string;
+  name?: string;
+  surname?: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+const UserContext = createContext<UserContextType | null>(null);
+
+export function UserProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUserState] = useState<User | null>(null);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('user');
+
+    if (savedUser) {
+      setUserState(JSON.parse(savedUser))
+    }
+  }, []);
+
+  const setUser = (user: User | null) => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+    setUserState(user);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('Something went wrong with the user constext');
+  }
+  return context;
+}
