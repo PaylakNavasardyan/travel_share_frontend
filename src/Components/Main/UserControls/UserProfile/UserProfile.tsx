cd7324f7f88c3173b77df07a289ec0decc3aa9aa
Paylak Navasardyan <paylak2002n@gmail.com>
2026-01-26 23:13:49 +0400

    Add initial profile picture settings to user profile

--- src/Components/Main/UserControls/UserProfile/UserProfile.tsx
+++ src/Components/Main/UserControls/UserProfile/UserProfile.tsx
@@ -2,25 +2,44 @@ import React from 'react'
 import classes from './UserProfille.module.css'
 import { useUser } from '../../../../context/UserContext'
 import { GoChevronLeft as GoChevronLeftIcon } from "react-icons/go";
+import { API_URL } from '../../../../http/index' 
+import { useNavigate } from 'react-router-dom';
 
 export default function UserProfile() {
   const GoChevronLeft = GoChevronLeftIcon as unknown as React.FC<{ className: string}>;
 
   const { user } = useUser();
-  const nameFirtLetter = user?.userName[0].toUpperCase();
+
+  let nameFirtLetter = user?.userName[0].toUpperCase();
+  let profilePic_URL = user?.profilePicture;
+  let profilePic = `${API_URL}/api/user/profile/${profilePic_URL}`
+
+  const navigate = useNavigate();
 
   return (
     <div className={classes.profile}>
       <div className={classes.profileBar}>
         <div className={classes.icon}>
-          <div className={classes.backgroundForIcon}>
+          <div 
+            className={classes.backgroundForIcon}
+            onClick={() => navigate(-1)}
+          >
             <GoChevronLeft className={classes.goLeftIcon}/>
           </div>
         </div>
 
         <div className={classes.profileInfo}>
           <div className={classes.profileImage}>
-            <p className={classes.nameLetter}>{nameFirtLetter}</p>
+            {
+              profilePic_URL 
+                ? 
+                <img 
+                  className={classes.profilePic}
+                  src={profilePic}
+                  alt="Profile-Picture" />
+                : 
+                <p className={classes.nameLetter}>{nameFirtLetter}</p> 
+            }
           </div>
 
           <div className={classes.personalInfo}>
