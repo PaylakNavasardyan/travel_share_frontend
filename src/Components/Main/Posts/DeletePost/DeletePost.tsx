138b70116a7763001633b2622acb2fc40e9e9832
Paylak Navasardyan <paylak2002n@gmail.com>
2026-03-11 21:04:42 +0400

    feat: implement DeletePost component with full styling

---
+++ src/Components/Main/Posts/DeletePost/DeletePost.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import classes from './DeletePost.module.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import $api from '../../../../http';
+
+export default function DeletePost() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+  
+  const navigate = useNavigate();
+  
+  const deletePost = async() => {
+    try {
+      const postId = location.state?.postId;
+      let response = await $api.delete(`/api/posts/${postId}`);
+      
+      if (response.status == 204) {
+        alert('Your post has been deleted');
+        navigate('/travel-share', { state: { refresh: true } });
+      } else {
+        alert('Something went wrong');
+        navigate('/my-profile')
+      } 
+    }catch(error)  {
+      console.log(error)
+    };
+  }
+    
+  return (
+    <div className={classes.deleteModal}>
+      <div className={classes.deleteBody}>
+        <p className={classes.deleteText}>Are you sure you want to delete this post?</p>
+
+        <div className={classes.buttons}>
+          <button 
+            className={`${classes.button} ${classes.cancelButton}`}
+            onClick={() => navigate(-1)}    
+          >Cancel</button>
+
+          <button 
+            className={`${classes.button} ${classes.deleteButton}`}
+            onClick={deletePost}
+          >Delete Post</button>
+        </div>
+      </div>
+    </div>
+  )
+}
