4b3d2a4f6cfacfefc21df5010b74c07ce00a298f
Paylak Navasardyan <paylak2002n@gmail.com>
2026-03-18 17:42:24 +0400

    chore: deduplicate modal post code and styles

--- src/Components/Main/Posts/AllPosts.tsx
+++ src/Components/Main/Posts/AllPosts.tsx
@@ -13,8 +13,8 @@ import {
   TbPlayerTrackPrevFilled as TbPlayerTrackPrevFilledIcon, 
   TbPlayerTrackNextFilled as TbPlayerTrackNextFilledIcon
 } from "react-icons/tb";
-import { IoMdClose as IoMdCloseIcon } from "react-icons/io";
 import PostActions from './PostReactions/PostActions';
+import PostModal from './ModalPosts/PostModal';
 
 export default function AllPosts() {
   const GoComment = GoCommentIcon as unknown as React.FC<{className: string}>;
@@ -23,14 +23,12 @@ export default function AllPosts() {
   const GoDotFill = GoDotFillIcon as unknown as React.FC<{ className: string }>;
   const TbPlayerTrackPrevFilled =TbPlayerTrackPrevFilledIcon as unknown as React.FC<{ className: string }>
   const TbPlayerTrackNextFilled = TbPlayerTrackNextFilledIcon as unknown as React.FC<{ className: string }>
-  const IoMdClose = IoMdCloseIcon as unknown as React.FC<{ className: string}>;
 
   const [posts, setPosts] = useState<Post[]>([]);
   const [postername, setPostername] = useState<string[]>([]);
   const [posterImage, setPosterImage] = useState<string[]>([]);
   const [firstLetter, setFirstLetter] = useState<string[]>([]);
   const [currentIndex, setCurrentIndex] = useState<{ [key: string]: number }>({});
-  const [selectedIndex, setSelectedIndex] = useState<{ [key: string]: number }>({});
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(false);
@@ -41,15 +39,6 @@ export default function AllPosts() {
 
   const selectedPost = posts.find(post => post._id === id);
 
-  const selIndex = selectedPost
-    ? selectedIndex[selectedPost._id] ?? 0
-    : 0;
-
-  const safeSelIndex =
-    selectedPost && selIndex >= selectedPost.media.length
-      ? 0
-      : selIndex;
-  
   const fetchPosts = async (shouldScroll = false) => {
     try {
       setLoading(true);
@@ -115,28 +104,6 @@ export default function AllPosts() {
         (prev[postId] ?? 0) - 1
       }
     ))
-  };
-
-  const nextModalSlide = (postId: string, mediaLength: number) => {
-    setSelectedIndex(prev => {
-      const current = prev[postId] ?? 0;
-
-      return {
-        ...prev,
-        [postId]: (current + 1) % mediaLength
-      };
-    });
-  };
-
-  const prevModalSlide = (postId: string, mediaLength: number) => {
-    setSelectedIndex(prev => {
-      const current = prev[postId] ?? 0;
-
-      return {
-        ...prev,
-        [postId]: current === 0 ? mediaLength - 1 : current - 1
-      };
-    });
   };
 
   return (
@@ -236,80 +203,11 @@ export default function AllPosts() {
       })}
 
       {selectedPost && (
-        <div className={classes.modalOverlay}>
-          <div
-            className={classes.modalCloseBorder}
-            onClick={() => navigate(-1)}
-          >
-            <IoMdClose className={classes.modalCloseIcon}/>
-          </div>
-
-          <div className={classes.modalContentBackground}>
-            <div
-              className={classes.modalContent}
-              onClick={(e) => e.stopPropagation()}
-            >
-              
-              <div className={selectedPost.media.length > 1 ? classes.fewItems : classes.oneItem}>
-
-                {selectedPost && selectedPost.media.length > 0 && (
-                  <img
-                    className={classes.postMedia}
-                    src={`${API_URL}/api/posts/media/${selectedPost.media[safeSelIndex].url}`}
-                    alt="Post Media"
-                  />
-                )}
-
-                {selectedPost.media.length > 1 && (
-                  <div
-                    className={`${classes.slideButtonBack} ${classes.left}`}
-                    onClick={() =>
-                      prevModalSlide(selectedPost._id, selectedPost.media.length)
-                    }
-                  >
-                    <GoChevronLeft className={classes.slideButton} />
-                  </div>
-                )}
-
-                {selectedPost.media.length > 1 && (
-                  <div
-                    className={`${classes.slideButtonBack} ${classes.right}`}
-                    onClick={() =>
-                      nextModalSlide(selectedPost._id, selectedPost.media.length)
-                    }
-                  >
-                    <GoChevronRight className={classes.slideButton} />
-                  </div>
-                )}
-
-              </div>
-  
-              <div className={classes.modalPostInfo}>
-                <div className={classes.modalPostUserData}>
-                  {selectedPost.user.profilePicture ? (
-                    <img
-                      className={classes.profilePic}
-                      src={`${API_URL}/api/user/profile/${selectedPost.user.profilePicture}`}
-                      alt="Profile-Picture"
-                    />
-                  ) : (
-                    <>
-                      <div className={classes.letter}>
-                        <p className={classes.firstletter}>
-                          {selectedPost.user.username[0].toUpperCase()}
-                        </p>
-                      </div>
-                    </>
-                  )}
-
-                  <span className={classes.username}>@{selectedPost.user.username}</span>
-                </div>
-                  <span className={classes.description}>{selectedPost.description}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}    
+        <PostModal
+          post={selectedPost}
+          apiUrl={API_URL}
+        />
+      )}
 
       <div className={classes.paginationGuide}>
         <button
