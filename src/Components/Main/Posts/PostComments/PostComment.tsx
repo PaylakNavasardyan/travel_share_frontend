638989ef0309e8206e9f1e495117e0f40b258997
Paylak Navasardyan <paylak2002n@gmail.com>
2026-03-25 18:39:26 +0400

    Update initial settings in Comment component

--- src/Components/Main/Posts/PostComments/PostComment.tsx
+++ src/Components/Main/Posts/PostComments/PostComment.tsx
@@ -1,26 +1,53 @@
 import React, { useState } from 'react';
 import classes from './PostComment.module.css';
 import { IoIosSend as IoIosSendIcon } from "react-icons/io";
+import $api from '../../../../http';
+import { CommentType } from '../../../../types/comment';
 
-export default function PostComment() {
-    const IoIosSend = IoIosSendIcon as unknown as React.FC<{ className : string }>;
+interface Props {
+  postId: string;
+  onAddComment: (comment: CommentType) => void;
+}
 
-    const [comment, setComment] = useState<string>('');   
+export default function PostComment({ postId, onAddComment }: Props) {
+  const IoIosSend = IoIosSendIcon as unknown as React.FC<{ className: string }>;
+
+  const [comment, setComment] = useState<string>('');
+
+  const handleComment = async () => {
+    if (!comment.trim()) return;
+
+    try {
+        const response = await $api.post('/api/comment/create', {
+            postId,
+            content: comment
+        });
+
+        const newComment = response.data.data; 
+
+        onAddComment(newComment);
+        setComment('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className={classes.commentArea}>
-        <textarea
-            className={comment.length > 0 ? classes.existedComment : classes.comment} 
-            placeholder='Add a comment...'
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-        >
-        </textarea>
+      <textarea
+        className={comment.length > 0 ? classes.existedComment : classes.comment}
+        placeholder='Add a comment...'
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
 
-        <div className={comment.length > 0 ? classes.existedIconArea : classes.iconArea}>
-            <span className={classes.postComment}>Post</span>
-            <IoIosSend className={classes.sendIcon} />    
-        </div>
+      <div
+        className={comment.length > 0 ? classes.existedIconArea : classes.iconArea}
+        onClick={handleComment}
+      >
+        <span className={classes.postComment}>Post</span>
+        <IoIosSend className={classes.sendIcon} />
+      </div>
     </div>
-  )
+  );
 }
