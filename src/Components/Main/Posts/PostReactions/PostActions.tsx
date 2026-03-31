92c822589cc2f52432f633f4a86c7ba1c31b06cc
Paylak Navasardyan <paylak2002n@gmail.com>
2026-03-31 16:25:38 +0400

    feat(posts): integrate useReaction hook into PostActions

--- src/Components/Main/Posts/PostReactions/PostActions.tsx
+++ src/Components/Main/Posts/PostReactions/PostActions.tsx
@@ -1,6 +1,6 @@
 import { SlLike as SILikeIcon, SlDislike as SlDislikeIcon } from "react-icons/sl";
-import { usePostReaction } from "./usePostReaction";
 import classes from './PostActions.module.css';
+import { useReaction } from "../UseReaction/useReaction";
 
 interface Props {
   postId: string;
@@ -13,7 +13,7 @@ export default function PostActions({ postId, likeCount, dislikeCount, reaction 
   const SILike = SILikeIcon as unknown as React.FC<{ className: string }>;
   const SIDislike = SlDislikeIcon as unknown as React.FC<{ className: string }>;
 
-  const { likes, dislikes, reaction: myreaction, handleReaction } = usePostReaction(postId, {
+  const { likes, dislikes, reaction: myreaction, handleReaction } = useReaction('post', postId, {
     likeCount,
     dislikeCount,
     userReaction: reaction,
