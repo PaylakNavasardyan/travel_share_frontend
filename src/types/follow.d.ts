e6acf1cfdb61ad7d501461250b1d5481403168df
Paylak Navasardyan <paylak2002n@gmail.com>
2026-04-27 23:43:41 +0400

    feat: add Follow type definition

---
+++ src/types/follow.d.ts
@@ -0,0 +1,11 @@
+type Follow = {
+    followId: string,
+    followedAt: string,
+    id: string,
+    name?: string,
+    surname?: string,
+    username: string,
+    profilePicture: string
+}
+
+export default Follow
